50fb905be84441c5a498173a4b452b635d63202a
Damien Marchal <damien.marchal@univ-lille1.fr>
2019-06-29 01:19:49 +0200

    [SofaPython3] Update sphinx-stubs

---
+++ docs/sphinx-stubs/Sofa/Types/__init__.py
@@ -0,0 +1,42 @@
+"""
+           Default types available in Sofa
+           -----------------------
+
+           .. autosummary::
+               Sofa.Types.Vec3
+               Sofa.Types.BoundingBox
+               ...
+
+       """
+from typing import *
+from array import array
+from typing import Iterable as iterable
+from typing import Iterator as iterator
+from numpy import float64
+_Shape = Tuple[int, ...]
+__all__  = [
+"BoundingBox"
+]
+class BoundingBox():...
+class BoundingBox():
+    def getCenter(self) -> list: 
+        """
+returns the center of the bbox
+"""
+    def getMax(self) -> list: 
+        """
+Returns a list (by copy) of the max bbox position
+"""
+    def getMin(self) -> list: 
+        """
+Returns a list (by copy) of the min bbox position
+"""
+    def setMax(self, arg0: list) -> None: 
+        """
+sets the max bbox position from a python list
+"""
+    def setMin(self, arg0: list) -> None: 
+        """
+sets the min bbox position from a python list
+"""
+    pass
---
+++ docs/sphinx-stubs/SofaRuntime/__init__.py
@@ -0,0 +1,11 @@
+from typing import *
+from array import array
+from typing import Iterable as iterable
+from typing import Iterator as iterator
+from numpy import float64
+_Shape = Tuple[int, ...]
+__all__  = [
+"importPlugin"
+]
+def importPlugin(arg0: str) -> bool:
+    pass
