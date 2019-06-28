56e529e1c0f68d759702751456d661e6db9dd09c
Damien Marchal <damien.marchal@univ-lille1.fr>
2019-06-29 01:19:12 +0200

    [SofaPython3] Update Sofa directory.

--- bindings/Sofa/src/SofaPython3/Sofa/Module_Sofa.cpp
+++ bindings/Sofa/src/SofaPython3/Sofa/Module_Sofa.cpp
@@ -9,21 +9,35 @@ namespace sofapython3
 /// The first parameter must be named the same as the module file to load.
 PYBIND11_MODULE(Sofa, m)
 {
+    m.doc() = R"doc(
+              Sofa binding
+              -----------------------
+
+              Example of use:
+                .. code-block:: python
+
+                   import Sofa
+
+                   n = Sofa.Core.Node("MyNode"")
+                   n.addChild("Node2")
+                   n.addObject("MechanicalObject", name="dofs")
+
+                   Sofa.Simulation.init(root)
+                   Sofa.Simulation.print(root)
+
+              Submodules:
+                .. autosummary::
+                  :toctree: _autosummary
+
+                  Sofa.Core
+                  Sofa.Simulation
+                  Sofa.Types
+                  Sofa.Helper
+             )doc";
     py::module core = addSubmoduleCore(m);
     py::module helper = addSubmoduleHelper(m);
     py::module simulation = addSubmoduleSimulation(m);
     py::module types = addSubmoduleTypes(m);
-
-    /// Import into the Sofa main package the class from theyr sub-module if they can be instantiated
-    m.add_object("Controller", core.attr("Controller"));
-    m.add_object("ForceField", core.attr("ForceField"));
-    m.add_object("Node", core.attr("Node"));
-
-    m.add_object("msg_info", helper.attr("msg_info"));
-    m.add_object("msg_warning", helper.attr("msg_warning"));
-    m.add_object("msg_error", helper.attr("msg_error"));
-    m.add_object("msg_fatal", helper.attr("msg_fatal"));
-    m.add_object("msg_deprecated", helper.attr("msg_deprecated"));
 }
 
 } ///namespace sofapython3
