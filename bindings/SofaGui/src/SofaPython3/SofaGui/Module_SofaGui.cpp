c698f57f4f751f3c4cb9cdb13e5bd3afe83dc336
Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
2019-07-08 16:10:36 -0400

    Adds the Sofa GUI bindings to the Sofa package.

--- bindings/SofaGui/src/SofaPython3/SofaGui/Module_SofaGui.cpp
+++ bindings/SofaGui/src/SofaPython3/SofaGui/Module_SofaGui.cpp
@@ -6,21 +6,27 @@ namespace py = pybind11;
 
 namespace sofapython3 {
 /// The first parameter must be named the same as the module file to load.
-PYBIND11_MODULE(SofaGui, m) {
+PYBIND11_MODULE(Gui, m) {
 
     m.doc() = R"doc(
-            SofaGui
+            Sofa.Gui
             -----------------------
 
             Example of use:
 
                 .. code-block:: python
+                    import Sofa.Gui
 
-                    from SofaGui import GUIManager
-                    supported_gui = GUIManager.ListSupportedGUI()
-                    print (supported_gui)
+                    supported_gui = Sofa.Gui.GUIManager.ListSupportedGUI(",")
+                    print ("Supported GUIs are " + supported_gui)
 
-                .. automodule:: SofaGui
+                    Sofa.Gui.GUIManager.Init("gui_script_example")
+                    Sofa.Gui.GUIManager.createGUI(root)
+                    Sofa.Gui.GUIManager.MainLoop(root)
+                    Sofa.Gui.GUIManager.closeGUI()
+
+
+                .. automodule:: Gui
                     :toctree: _autosummary
                     :members:
              )doc";
