02ccba4d72dd685a2c540a9c9d5bf25432548099
Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
2019-07-15 10:08:13 -0400

    Move data and plugin repository global variables to SofaRuntime

--- bindings/Sofa/src/SofaPython3/Sofa/Helper/System/Submodule_System.cpp
+++ bindings/Sofa/src/SofaPython3/Sofa/Helper/System/Submodule_System.cpp
@@ -1,8 +1,6 @@
 #include "Submodule_System.h"
 #include "Binding_FileRepository.h"
 
-#include <sofa/helper/system/FileRepository.h>
-
 namespace sofapython3
 {
 
@@ -11,9 +9,6 @@ void moduleAddSystem(py::module &m)
     auto system = m.def_submodule("System");
 
     moduleAddFileRepository(system);
-
-    m.add_object("DataRepository", py::cast(&sofa::helper::system::DataRepository));
-    m.add_object("PluginRepository", py::cast(&sofa::helper::system::PluginRepository));
 }
 
 } ///namespace sofapython3
