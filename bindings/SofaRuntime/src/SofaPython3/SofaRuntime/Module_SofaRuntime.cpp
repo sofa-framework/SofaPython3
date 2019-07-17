82d164d2ddda5b3aaf07d671786f04f6849dfa66
Damien Marchal <damien.marchal@univ-lille1.fr>
2019-07-17 13:59:48 +0200

    [SofaPython3] Partially Fix the SofaRuntime module
    Because of removal of Sofa module (it is now a package) we need to
    import things more specifically when import("Sofa").
    
    The two line with add_object are not working for unknow reason. We should
    fix that properly.But it will be in a future PR.

--- bindings/SofaRuntime/src/SofaPython3/SofaRuntime/Module_SofaRuntime.cpp
+++ bindings/SofaRuntime/src/SofaPython3/SofaRuntime/Module_SofaRuntime.cpp
@@ -91,7 +91,7 @@ PYBIND11_MODULE(SofaRuntime, m) {
     //    sofa::simulation::setSimulation(new DAGSimulation());
 
     /// We need to import the project dependencies
-    py::module::import("Sofa");
+    py::module::import("Sofa.Core");
 
     /// Check if there is already a SceneLoaderFactory. In case not load it.
     if( !SceneLoaderFactory::getInstance()->getEntryFileExtension("py3") )
@@ -105,8 +105,8 @@ PYBIND11_MODULE(SofaRuntime, m) {
         return simpleapi::importPlugin(name);
     });
 
-    m.add_object("DataRepository", py::cast(&sofa::helper::system::DataRepository));
-    m.add_object("PluginRepository", py::cast(&sofa::helper::system::PluginRepository));
+    //m.add_object("DataRepository", py::cast(&sofa::helper::system::DataRepository));
+    //m.add_object("PluginRepository", py::cast(&sofa::helper::system::PluginRepository));
 
     addSubmoduleInput(m);
     addSubmoduleTimer(m);
