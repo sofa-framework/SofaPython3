bc3dde1439f9d1a76249dd8bb1616f74b02a1929
Damien Marchal <damien.marchal@univ-lille1.fr>
2019-08-27 13:24:00 +0200

    [SofaPython3] Remove old copyright that was wrong.

--- bindings/Sofa/src/SofaPython3/Sofa/Core/Binding_BaseCamera.cpp
+++ bindings/Sofa/src/SofaPython3/Sofa/Core/Binding_BaseCamera.cpp
@@ -1,25 +1,3 @@
-/*********************************************************************
-Copyright 2019, Inria, CNRS, University of Lille
-
-This file is part of runSofa2
-
-runSofa2 is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-runSofa2 is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with sofaqtquick. If not, see <http://www.gnu.org/licenses/>.
-*********************************************************************/
-/********************************************************************
- Contributors:
-    - damien.marchal@univ-lille.fr
-********************************************************************/
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/detail/init.h>
