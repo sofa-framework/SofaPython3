79f8b83186b29701d43172a05ac4c50518d9d747
Stefan Escaida Navarro <stefan.escaida-navarro@inria.fr>
2020-04-27 11:43:52 +0200

    [SPLIB] Add meshing utilities with CGAL and GMSH

---
+++ splib/meshing/__init__.py
@@ -0,0 +1,17 @@
+"""
+Meshing module
+----------------
+Meshing Functionality using python3-bindings Gmsh and CGAL.
+
+Submodules:
+  .. autosummary::
+    :toctree: _autosummary
+
+    splib.meshing.gmsh_sofa
+    splib.meshing.cgal_sofa
+
+
+
+"""
+__all__=["gmsh_sofa", "cgal_sofa"] 
+
