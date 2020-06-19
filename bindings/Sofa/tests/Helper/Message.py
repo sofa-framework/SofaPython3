98220635b63e2f2185613d3b30e5e58a32e1d0de
Bruno Marques <bmarques68@gmail.com>
2020-06-19 17:52:15 +0200

    WIP: Add MessageHandler context manager class

--- bindings/Sofa/tests/Helper/Message.py
+++ bindings/Sofa/tests/Helper/Message.py
@@ -4,6 +4,23 @@ import Sofa
 import Sofa.Helper
 import unittest
 
+class MsgHandler(Sofa.Helper.MessageHandler):
+    def __init__(self):
+        Sofa.Helper.MessageHandler.__init__(self)
+
+    def process(msg):
+        print ("GOT MESSAGE!")
+        print("type: ")
+        print(msg.type)
+        print("sender: ")
+        print(msg.sender)
+        print("component: ")
+        print(msg.component.typeName())
+        print(msg.component.getName())
+        print("message:")
+        print(msg.message)
+
+
 class Test(unittest.TestCase):
     def test_messages(self):
         """Test that the message are correctly sended and does not generates exceptions"""
@@ -14,4 +31,13 @@ class Test(unittest.TestCase):
             f("Simple message with attached source info", "sourcefile.py", 10)
             f(Sofa.Core.Node("node"), "Simple message to an object")
             f(Sofa.Core.Node("node"), "Simple message to an object with attached source info", "sourcefile.py", 10)
+
+
+
+    def test_messageHandler(self):
+        with MsgHandler():
+            Sofa.Helper.msg_info("plop")
+            Sofa.Helper.msg_error("pouet")
+        Sofa.Helper.msg_warning("coucou")
+
 
