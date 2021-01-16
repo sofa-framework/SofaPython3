097bfff88b5087e590b427d74aeddf835b440a49
Hugo Talbot <hugo.talbot@sofa-framework.org>
2021-01-16 10:17:57 +0100

    update license 1/2

--- Plugin/src/SofaPython3/DataCache.h
+++ Plugin/src/SofaPython3/DataCache.h
@@ -1,21 +1,24 @@
-/*********************************************************************
-Copyright 2019, CNRS, University of Lille, INRIA
-
-This file is part of sofaPython3
-
-sofaPython3 is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-sofaPython3 is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with sofaqtquick. If not, see <http://www.gnu.org/licenses/>.
-*********************************************************************/
+/******************************************************************************
+*                              SofaPython3 plugin                             *
+*                  (c) 2019 CNRS, University of Lille, INRIA                  *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: see Authors.txt                                                    *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
 /********************************************************************
  Contributors:
     - damien.marchal@univ-lille.fr
@@ -58,4 +61,3 @@ py::array SOFAPYTHON3_API getPythonArrayFor(BaseData* d);
 void trimCache();
 
 } /// sofapython3
-
--- Plugin/src/SofaPython3/PythonTestExtractor.cpp
+++ Plugin/src/SofaPython3/PythonTestExtractor.cpp
@@ -1,3 +1,24 @@
+/******************************************************************************
+*                              SofaPython3 plugin                             *
+*                  (c) 2019 CNRS, University of Lille, INRIA                  *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: see Authors.txt                                                    *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
 #include <SofaPython3/PythonTestExtractor.h>
 #include <SofaPython3/PythonEnvironment.h>
 
