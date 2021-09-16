04f1489b9de4b2f527db7fb7378cb4db549e57e7
Damien Marchal <damien.marchal@univ-lille1.fr>
2021-09-16 12:49:41 +0200

    [Binding/Sofa.Core] FIX  Node iterators. (#171)
    * [SofaPython3] Add the contains operator to Node iterators.
    
    
    
    So we can now write:
    def createScene(root):
    	if "MyObject" in root.objects:
    		pass
    	if "MyChild" in root.children:
    		pass
    	if "MyParent" in root.parents:
    		pass
    
    * [SofaPython3] FIX the NodeIterator.
    
    The actually implmentation of NodeIterator is broken.
    Eg:
       - the __getitem__ by name was always returning a BaseObject whatever the iterable was on object, children or parents
       - the remove_at method was actually always deleting a Child whatever the iterable was on object, children or parents
    There is also no __contains__ operator which prevent to write pythonic
    if "AAA" in node.objects:
       pass.
    
    This PR:
    	- fix the broken implementation on remove_at and __getitem__ by adding lambda functions allowing to implement per-type behavior.
    	- add tests for the fixed features
    	- implements the __contains__ method and dedicated tests.
    	- update the corresponding docstring
    
    In this PR I also added

--- bindings/Sofa/src/SofaPython3/Sofa/Core/Binding_NodeIterator.h
+++ bindings/Sofa/src/SofaPython3/Sofa/Core/Binding_NodeIterator.h
@@ -26,19 +26,42 @@
 
 namespace sofapython3 {
 
-class NodeIterator {
+/// Generic implementation to iterator over the different structures of a Node.
+/// This allows to implement pythonic solution as in:
+///     for object in node.objects:
+///          print(object.name)
+///
+/// or
+///     for child in node.children:
+///          print(child.name)
+///
+/// there is also support for several function like:
+///     len(node.children)
+///     if "MyName" in node.children
+///     node.children.remove_at(index)
+///
+/// The implementation is generic by the use of lamda function allowing to specify which
+/// data structure is actually iterated.
+class NodeIterator
+{
 public:
     sofa::core::sptr<sofa::simulation::Node> owner;
     size_t     index=0;
     std::function<size_t (sofa::simulation::Node *)> size ;
     std::function<sofa::core::sptr<sofa::core::Base> (sofa::simulation::Node *, size_t)> get ;
+    std::function<sofa::core::Base* (const sofa::simulation::Node *, const std::string&)> get_by_name ;
+    std::function<void (sofa::simulation::Node*, unsigned int)> remove_at;
 
     NodeIterator(sofa::core::sptr<sofa::simulation::Node> owner_,
                  std::function<size_t (sofa::simulation::Node *)> size_,
-                 std::function<sofa::core::sptr<sofa::core::Base> (sofa::simulation::Node *, size_t)> get_)
+                 std::function<sofa::core::sptr<sofa::core::Base> (sofa::simulation::Node *, size_t)> get_,
+                 std::function<sofa::core::Base* (const sofa::simulation::Node *, const std::string&)> get_by_name_,
+                 std::function<void (sofa::simulation::Node*, unsigned int)> remove_at_)
     {
         size = size_;
         get = get_;
+        get_by_name = get_by_name_;
+        remove_at = remove_at_;
         owner=owner_;
         index=0;
     }
