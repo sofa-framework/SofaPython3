e7940a34df033ed1d6dca99e6c4d688ce9abbdab
Frederick Roy <fredroy@users.noreply.github.com>
2021-09-20 12:18:57 +0200

    [Win/MSVC] Fix search of modules (#190)

--- Plugin/src/SofaPython3/PythonEnvironment.h
+++ Plugin/src/SofaPython3/PythonEnvironment.h
@@ -72,6 +72,7 @@ public:
     /// NB: can also be used for projects <projectDirectory>/*/python
     static void addPythonModulePathsForPlugins(const std::string& pluginsDirectory);    
     static void addPythonModulePathsForPluginsByName(const std::string& pluginName);
+    static void addPythonModulePathsFromSofaRoot();
 
     /// set the content of sys.argv.
     static void setArguments(const std::string& filename,
