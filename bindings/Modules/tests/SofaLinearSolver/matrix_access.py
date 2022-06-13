3dd8b97ce8fd24755ef5392a64ffbd34b12a2041
Alex Bilger <alexbilger0@gmail.com>
2022-06-13 13:40:51 +0200

    Remove unnecessary controller

--- bindings/Modules/tests/SofaLinearSolver/matrix_access.py
+++ bindings/Modules/tests/SofaLinearSolver/matrix_access.py
@@ -15,46 +15,35 @@ class Test(unittest.TestCase):
         root.addObject('RequiredPlugin', name='Sofa.Component.Engine.Select')
         root.addObject('RequiredPlugin', name='Sofa.Component.Constraint.Projective')
         root.addObject('RequiredPlugin', name='Sofa.Component.SolidMechanics.FEM.Elastic')
+        root.addObject('RequiredPlugin', name='Sofa.Component.Mass')
 
         root.addObject('EulerImplicitSolver', rayleighStiffness="0.1", rayleighMass="0.1")
-        linear_solver = root.addObject('SparseLDLSolver', applyPermutation="false", template=linear_solver_template)
+        linear_solver = root.addObject('SparseLDLSolver', name='linear_solver', applyPermutation="false", template=linear_solver_template)
 
         root.addObject('MechanicalObject', name="DoFs")
         root.addObject('UniformMass', name="mass", totalMass="320")
         root.addObject('RegularGridTopology', name="grid", nx="4", ny="4", nz="20", xmin="-9", xmax="-6", ymin="0", ymax="3", zmin="0", zmax="19")
-        root.addObject('BoxROI', name="box", box="-10 -1 -0.0001  -5 4 0.0001")
+        root.addObject('BoxROI', name="box", box=[-10, -1, -0.0001, -5, 4, 0.0001])
         root.addObject('FixedConstraint', indices="@box.indices")
         root.addObject('HexahedronFEMForceField', name="FEM", youngModulus="4000", poissonRatio="0.3", method="large")
-
-        matrix_accessor = root.addObject(MatrixAccessController('MatrixAccessor', name='matrixAccessor', linear_solver=linear_solver))
 
         Sofa.Simulation.init(root)
         Sofa.Simulation.animate(root, 0.0001)
 
-        return matrix_accessor.system_matrix
+        return root
 
     def test_matrix_access_blocks_scalar(self):
-        A = self.simulate_beam("CompressedRowSparseMatrixd")
+        root = self.simulate_beam("CompressedRowSparseMatrixd")
+        A = root.linear_solver.A()
 
         self.assertEqual(A.ndim, 2)
         self.assertEqual(A.shape, (960, 960))
         self.assertEqual(A.nnz, 52200)
 
     def test_matrix_access_blocks3x3(self):
-        A = self.simulate_beam("CompressedRowSparseMatrixMat3x3d")
+        root = self.simulate_beam("CompressedRowSparseMatrixMat3x3d")
+        A = root.linear_solver.A()
 
         self.assertEqual(A.ndim, 2)
         self.assertEqual(A.shape, (960, 960))
         self.assertEqual(A.nnz, 41629)
-
-
-class MatrixAccessController(Sofa.Core.Controller):
-
-    def __init__(self, *args, **kwargs):
-        Sofa.Core.Controller.__init__(self, *args, **kwargs)
-        self.linear_solver = kwargs.get("linear_solver")
-
-    def onAnimateEndEvent(self, event):
-        self.system_matrix = self.linear_solver.A()
-        self.rhs = self.linear_solver.b()
-        self.solution = self.linear_solver.x()
