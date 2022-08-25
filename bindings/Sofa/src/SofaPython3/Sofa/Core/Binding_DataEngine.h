1a116e1dfca2a4b09a6cd86eefbe1724f33a87d4
Alex Bilger <alexbilger0@gmail.com>
2022-08-25 15:02:26 +0200

    Revert "[Core] Make some functions non-virtual"
    This reverts commit 4470f4aa11aceebd2870679e00d52c2a95a389a1.

--- bindings/Sofa/src/SofaPython3/Sofa/Core/Binding_DataEngine.h
+++ bindings/Sofa/src/SofaPython3/Sofa/Core/Binding_DataEngine.h
@@ -31,6 +31,8 @@ public:
     SOFA_CLASS(DataEngine_Trampoline, sofa::core::DataEngine);
     void init() override;
     void doUpdate() override;
+
+    std::string getClassName() const override;
 };
 
 void moduleAddDataEngine(pybind11::module &m);
