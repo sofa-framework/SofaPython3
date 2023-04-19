9151f456907f809814157261421b3f3eedf74f73
Alex Bilger <alexbilger0@gmail.com>
2023-04-19 18:34:35 +0200

    Remove MappingGeometricStiffnessForceField from example

--- examples/access_compliance_matrix.py
+++ examples/access_compliance_matrix.py
@@ -32,7 +32,6 @@ def createScene(root):
     root.addObject("EdgeSetGeometryAlgorithms", drawEdges=True)
     root.addObject("FixedConstraint", indices=[0])
     root.addObject("DiagonalMass", name="mass", totalMass="1e-3")
-    root.addObject("MappingGeometricStiffnessForceField", mapping="@./extensionsNode/distanceMapping")
 
     ext = root.addChild("extensionsNode")
     ext.addObject("MechanicalObject", template="Vec1d", name="extensionsDOF")
