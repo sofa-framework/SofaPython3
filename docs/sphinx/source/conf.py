905e5dcbb0101e0f807972b026dff6bf62e3d811
Damien Marchal <damien.marchal@univ-lille1.fr>
2023-05-24 15:14:22 +0200

    Remove the automock of Sofa

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -213,7 +213,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-autodoc_mock_imports = ['numpy', 'Sofa', 'SofaRuntime', 'SofaExporter', 'SofaTypes', 'splib']
+autodoc_mock_imports = ['numpy']
 sys.modules.update((mod_name, Mock()) for mod_name in autodoc_mock_imports)
 
 
