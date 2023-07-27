92ae8de8dcd9f45290f0dbff29ca9547f34cebde
Alex Bilger <alxbilger@users.noreply.github.com>
2023-07-27 03:02:46 +0200

    Follow changes from SOFA #3889 (#357)

--- Plugin/src/SofaPython3/Prefab.cpp
+++ Plugin/src/SofaPython3/Prefab.cpp
@@ -69,7 +69,7 @@ void Prefab::reinit()
     doReInit();
 
     /// Beurk beurk beurk
-    sofa::simulation::getSimulation()->initNode(this);
+    sofa::simulation::node::init(this);
     execute<VisualInitVisitor>(sofa::core::execparams::defaultInstance());
 }
 
