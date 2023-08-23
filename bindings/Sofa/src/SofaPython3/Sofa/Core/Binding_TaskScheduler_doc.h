e3145fa7542c59d5cbe9acf4565dbe9d3ef5566e
Alex Bilger <alxbilger@users.noreply.github.com>
2023-08-23 22:46:56 +0200

    Binding to setup task scheduler (nb threads) (#366)
    * Binding to setup task scheduler (nb threads)
    
    * Update examples/taskScheduler.py
    
    Co-authored-by: Hugo <hugo.talbot@sofa-framework.org>
    
    ---------
    
    Co-authored-by: Hugo <hugo.talbot@sofa-framework.org>

---
+++ bindings/Sofa/src/SofaPython3/Sofa/Core/Binding_TaskScheduler_doc.h
@@ -0,0 +1,40 @@
+/******************************************************************************
+*                              SofaPython3 plugin                             *
+*                  (c) 2021 CNRS, University of Lille, INRIA                  *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+
+#pragma once
+
+namespace sofapython3::doc::taskscheduler
+{
+
+static auto init =
+    R"(
+    Initialize the main task scheduler with a number of threads.
+    )";
+
+static auto getThreadCount =
+    R"(
+    Returns the number of threads used by the main task scheduler.
+    )";
+
+static auto GetHardwareThreadsCount =
+    R"(
+    Assuming 2 concurrent threads by CPU core, return the number of CPU cores on the system
+    )";
+}
