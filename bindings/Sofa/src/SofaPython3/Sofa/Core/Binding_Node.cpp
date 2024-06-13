ba1bd54cd40f508ac424b51ca73a17975c9baeb3
Paul Baksic <30337881+bakpaul@users.noreply.github.com>
2024-06-13 14:35:29 +0200

    add method to check if node has ode solver (#420)
    * add method to check if node has ode solver
    
    * Add missing parts
    
    * Update bindings/Sofa/src/SofaPython3/Sofa/Core/Binding_Node_doc.h
    
    Co-authored-by: erik pernod <erik.pernod@gmail.com>
    
    * Update bindings/Sofa/src/SofaPython3/Sofa/Core/Binding_Node.cpp
    
    Co-authored-by: Frederick Roy <fredroy@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: erik pernod <erik.pernod@gmail.com>
    Co-authored-by: Frederick Roy <fredroy@users.noreply.github.com>

--- bindings/Sofa/src/SofaPython3/Sofa/Core/Binding_Node.cpp
+++ bindings/Sofa/src/SofaPython3/Sofa/Core/Binding_Node.cpp
@@ -575,6 +575,13 @@ py::object getMechanicalState(Node *self)
 }
 
 
+py::object hasODESolver(Node *self)
+{
+    const bool hasODE = self->solver.size() > 0;
+    return py::cast(hasODE);
+}
+
+
 py::object getMechanicalMapping(Node *self)
 {
     sofa::core::BaseMapping* mapping = self->mechanicalMapping.get();
@@ -648,6 +655,7 @@ void moduleAddNode(py::module &m) {
     p.def("getAsACreateObjectParameter", &getLinkPath, sofapython3::doc::sofa::core::Node::getAsACreateObjectParameter);
     p.def("detachFromGraph", &Node::detachFromGraph, sofapython3::doc::sofa::core::Node::detachFromGraph);
     p.def("getMass", &getMass, sofapython3::doc::sofa::core::Node::getMass);
+    p.def("hasODESolver", &hasODESolver, sofapython3::doc::sofa::core::Node::hasODESolver);
     p.def("getForceField", &getForceField, sofapython3::doc::sofa::core::Node::getForceField);
     p.def("getMechanicalState", &getMechanicalState, sofapython3::doc::sofa::core::Node::getMechanicalState);
     p.def("getMechanicalMapping", &getMechanicalMapping, sofapython3::doc::sofa::core::Node::getMechanicalMapping);
