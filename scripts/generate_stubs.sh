61c270999e7eb16b4c2330d55f8bfaba29a9dec3
Paul Baksic <paul.baksic@outlook.fr>
2024-09-19 18:12:47 +0200

    Generating stubs through cmake does not work as expected with ninja with a in build startegy and isnt working in install because the install order is random

--- scripts/generate_stubs.sh
+++ scripts/generate_stubs.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+usage() {
+    echo "Usage: generate_stubs.sh <sofa-python3-site-packages-dir> "
+    echo "This script automatically generates stub files for SofaPython3 modules"
+}
+
+if [ "$#" -eq 1 ]; then
+    WORK_DIR=$1
+else
+    usage; exit 1
+fi
+
+cd $WORK_DIR
+
+
 for module_name in *; do
     echo "Generating stubgen for $module_name"
     stubgen -p $module_name --include-docstrings  --inspect-mode
