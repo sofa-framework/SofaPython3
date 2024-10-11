1cbc85970777f39b22b35a4a80be2e7b9ec8ce44
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-10-11 18:33:25 +0200

    [Nix] initial packaging & flake

---
+++ package.nix
@@ -0,0 +1,53 @@
+{
+  cmake,
+  lib,
+  libsForQt5,
+  python3Packages,
+  sofa,
+  stdenv,
+}:
+
+stdenv.mkDerivation {
+  pname = "sofa-python3";
+  version = "24.06";
+
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./bindings
+      ./CMake
+      ./CMakeLists.txt
+      ./constants
+      ./docs
+      ./examples
+      ./Plugin
+      ./SofaPython3Config.cmake.in
+      ./splib
+      ./Testing
+    ];
+  };
+
+  nativeBuildInputs = [
+    cmake
+    libsForQt5.libqglviewer
+    libsForQt5.wrapQtAppsHook
+  ];
+  propagatedBuildInputs = [
+    python3Packages.pybind11
+    python3Packages.scipy
+    sofa
+  ];
+
+  # help locate FindQGLViewer.cmake
+  cmakeFlags = [
+    "-DCMAKE_MODULE_PATH=${sofa.src}/cmake/Modules"
+  ];
+
+  meta = {
+    description = "python plugin for Sofa offering a pythonic interface and python3 support";
+    homepage = "https://github.com/sofa-framework/SofaPython3";
+    license = lib.licenses.lgpl21Only;
+    maintainers = with lib.maintainers; [ nim65s ];
+    platforms = lib.platforms.unix ++ lib.platforms.windows;
+  };
+}
