1c06f95483f19ae8097c59e7ed6d48c03ffd1034
Damien Marchal <damien.marchal@univ-lille1.fr>
2024-10-17 16:57:35 +0200

    Use pybind11-stubgen & change script for stub generation

--- scripts/generate_stubs.sh
+++ scripts/generate_stubs.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: generate_stubs.sh <sofa-python3-site-packages-dir> "
+    echo "Usage: generate_stubs.sh <destination> <module_name> "
     echo "This script automatically generates stub files for SofaPython3 modules"
 }
 
-if [ "$#" -eq 1 ]; then
+if [ "$#" -ge 2 ]; then
     WORK_DIR=$1
 else
     usage; exit 1
 fi
 
-cd $WORK_DIR
+destination_dir=$1
+if [ ! -d "$destination_dir/$module_name/" ]; then
+    echo "Destination directory does not contains a module with name $module_name"
+    exit 1
+fi
 
 
-for module_name in *; do
-    echo "Generating stubgen for $module_name"
-    stubgen -v -p $module_name --include-docstrings  --inspect-mode --ignore-errors
+for module_name in "${@:2}"
+do
+    echo "Generating stubgen for $module_name in $destination_dir"
+    pybind11-stubgen $module_name -o out
     if [ -d "./out/$module_name/" ]; then
-      rsync -a ./out/$module_name/ ./$module_name/
+      rsync -a ./out/$module_name/ $destination_dir/$module_name/
     fi
-    rm -rf ./out
+    echo $"Resync terminated for copying '$module_name' in '$destination_dir/$module_name/'"
+    rm -rf ./out/$module_name
 done
+rm -rf "./out"
