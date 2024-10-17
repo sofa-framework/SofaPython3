cd3a3b75322182ff1dbe1245ddb3dfa69b17d6a8
Damien Marchal <damien.marchal@univ-lille1.fr>
2024-10-17 13:15:57 +0200

    Fix broken tests.

--- bindings/Sofa/tests/Core/ForceField.py
+++ bindings/Sofa/tests/Core/ForceField.py
@@ -41,6 +41,7 @@ def createParticle(node, node_name, use_implicit_scheme, use_iterative_solver):
 
 def rssffScene(use_implicit_scheme=True, use_iterative_solver=True):
     node = Sofa.Core.Node("root")
+    node.addObject('DefaultAnimationLoop')
     node.addObject("RequiredPlugin", name="Sofa.Component.StateContainer")
     node.addObject("RequiredPlugin", name="Sofa.Component.LinearSolver")
     node.addObject("RequiredPlugin", name="Sofa.Component.ODESolver")
