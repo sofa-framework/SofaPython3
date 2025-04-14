7234379085350cf3b3fbea6ac8c03fc2fe3ef80d
bakpaul <paul.baksic@outlook.fr>
2025-04-14 18:22:59 +0200

    Start work on visual and collision

--- stlib/entities/rigid/__parameters__.py
+++ stlib/entities/rigid/__parameters__.py
@@ -1,8 +1,10 @@
 from stlib.core.baseParameters import BaseParameters, Callable, Optional, dataclasses
+from stlib.geometry import GeometryParameters
 
 @dataclasses.dataclass
 class RigidParameters(BaseParameters):     
 
+    geometry : GeometryParameters
     mass : Optional[float] = None    
 
     def toDict(self):
