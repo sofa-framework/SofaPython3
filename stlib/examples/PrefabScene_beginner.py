31ee7a791cb082fcf48394d9eb63f0c963dd634c
bakpaul <paul.baksic@outlook.fr>
2025-04-14 10:29:32 +0200

    Created example scenes

---
+++ stlib/examples/PrefabScene_beginner.py
@@ -0,0 +1,35 @@
+
+class exportScene():
+    def __init__(self,name="rootNode"):
+        self.name = name
+
+    def addObject(self,type:str,**kwargs):
+        suffix = ""
+        for i in kwargs:
+            suffix += "," + str(i) + "=\"" + str(kwargs[i]) + "\""
+        print(self.name+".addObject(\"" + type + "\"" +  suffix + ")")
+
+    def addChild(self,name:str):
+        print(name + '=' + self.name+".addChild(\"" + name + "\")")
+        setattr(self,name,exportScene(name))
+        return getattr(self,name)
+
+    def __setattr__(self, key, value):
+        if(not(key == "name")):
+            print(self.__dict__["name"] + "." + key + " = " + str(value))
+            self.__dict__[key] = value
+        else:
+            self.__dict__[key] = value
+
+
+
+def createScene(root):
+   
+
+   return root
+
+
+if __name__=="__main__":
+   
+   
+   pass
