fc881002fbba1cc42206b54312b5c195676dd6ac
bakpaul <paul.baksic@outlook.fr>
2025-04-14 17:49:47 +0200

    Add beginner example

---
+++ stlib/entities/rigid/__init__.py
@@ -0,0 +1,1 @@
+from __rigid__ import *
---
+++ stlib/geometry/file.py
@@ -0,0 +1,24 @@
+from stlib.geometry import GeometryParameters
+from splib.topology.loader import loadMesh
+from Sofa.Core import Node
+
+class FileParameters(GeometryParameters):
+    def __init__(self,  filename, dynamicTopology = False):
+
+        customGeom = FileParameters.createData(filename)
+        GeometryParameters.__init__(data = customGeom, dynamicTopology = dynamicTopology)
+
+    @staticmethod
+    def createData(filename) -> GeometryParameters.Data :
+        data = GeometryParameters.Data()
+        node = Sofa.Core.Node()
+        loadMesh(node, filename)
+        node.init()
+
+        data.positions = node.loader.position.value
+        data.edges = node.loader.edges.value
+        data.triangles = node.loader.triangles.value
+        data.tetrahedra = node.loader.tetras.value
+
+        return data
+        
