4c3890f7f59bcb9edd39ca71d17c94ca94b42fa8
bakpaul <paul.baksic@outlook.fr>
2025-04-14 11:31:18 +0200

    Move around files

---
+++ stlib/core/parameters.py
@@ -0,0 +1,24 @@
+import dataclasses
+from splib.core.utils import DEFAULT_VALUE
+
+#defaultValueType
+
+@dataclasses.dataclass
+class PrefabParameters(object): 
+    name : str = "Prefab"
+    kwargs : dict = dataclasses.field(default_factory=dict)
+
+    def __getattr__(self, name: str) :
+        if name == "__getstate__":
+            getattr(PrefabParameters, "__getstate__")
+        if name == "__setstate__":
+            getattr(PrefabParameters, "__setstate__")
+
+        try: 
+            a =  self.__getattribute__(name)
+        except Exception as e: 
+            return DEFAULT_VALUE
+        return a
+
+    def to_dict(self):
+        return dataclasses.asdict(self)
