73849a3277d85c227b94e86117adff5dd7840b19
bakpaul <paul.baksic@outlook.fr>
2025-04-14 10:57:28 +0200

    bootstrap examples

---
+++ stlib/examples/PrefabScene_expert.py
@@ -0,0 +1,14 @@
+from splib.helper import exportScene
+from stlib.misc.entity import Entity
+
+
+def createScene(root):
+    Entity.Deformable(Mesh,MechaProperties)
+    
+    return root
+
+
+if __name__=="__main__":
+    root = exportScene()
+    createScene(root)
+    pass
