b5efd73899d364ef24f0134ae4c12e8731e91867
bakpaul <paul.baksic@outlook.fr>
2025-04-14 15:18:53 +0200

    Add gemetries

---
+++ stlib/core/baseParameters.py
@@ -0,0 +1,13 @@
+import dataclasses
+from splib.core.utils import DEFAULT_VALUE
+
+import dataclasses
+from types import Callable, Optional
+
+@dataclasses.dataclass
+class BaseParameters(object): 
+    name : str = "Entity"
+    kwargs : dict = dataclasses.field(default_factory=dict)
+
+    def toDict(self):
+        return dataclasses.asdict(self)
