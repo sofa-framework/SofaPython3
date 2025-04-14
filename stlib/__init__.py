a6ff8859b6bec6853969d8c93d9ced273fc1ade4
bakpaul <paul.baksic@outlook.fr>
2025-04-14 10:03:50 +0200

    add folder for STLIB

---
+++ stlib/__init__.py
@@ -0,0 +1,4 @@
+"""SPLIB is now relocated at: https://github.com/SofaDefrost/STLIB, please clone and install the plugin to use it"""
+raise Exception("SPLIB is now relocated at: https://github.com/SofaDefrost/STLIB, please clone and install the plugin to use it")
+
+
