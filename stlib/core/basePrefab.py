d0c514b0129910c29d340b7775270f7339673d8d
hugtalbot <hugo.talbot@sofa-framework.org>
2025-04-15 13:53:42 +0200

    work on Tuesday : visual, geometry

--- stlib/core/basePrefab.py
+++ stlib/core/basePrefab.py
@@ -1,5 +1,29 @@
+import copy
 import Sofa.Core
-from baseParameters import BaseParameters
+from stlib.core.basePrefabParameters import BasePrefabParameters
+
+
+def addFromTypeName(self : Sofa.Core.Node, typeName, params = BasePrefabParameters, **kwargs):  
+    def findName(cname, node):
+        """Compute a working unique name in the node"""
+        rname = cname 
+        for i in range(0, len(node.children)):
+            if rname not in node.children:
+                return rname
+            rname = cname + str(i+1)
+        return rname
+
+    for k,v in kwargs.items():
+        if hasattr(params, k):
+            setattr(params, k, v)
+
+    params = copy.copy(params)
+    if params.name in self.children:
+        params.name = findName(params.name, self)
+
+    return typeName(parent = self, parameters=params) 
+
+Sofa.Core.Node.add = addFromTypeName
 
 
 class BasePrefab(Sofa.Core.Prefab):
@@ -9,3 +33,7 @@ class BasePrefab(Sofa.Core.Prefab):
 
     def __init__(self, params: BaseParameters):
         Sofa.Core.Prefab.__init__(self, name=params.name)
+
+    def localToGlobalCoordinates(pointCloudInput, pointCloudOutput):
+        raise NotImplemented("Send an email to Damien, he will help you. Guaranteed :)")
+        
