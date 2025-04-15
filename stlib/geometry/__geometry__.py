d5aa2bbe66da340d275d5bbb14d90cf973cecac6
bakpaul <paul.baksic@outlook.fr>
2025-04-15 15:03:42 +0200

    Add extracted geometry

--- stlib/geometry/__geometry__.py
+++ stlib/geometry/__geometry__.py
@@ -3,38 +3,52 @@ from stlib.core.baseParameters import BaseParameters, Optional, dataclasses, Any
 from splib.topology.dynamic import addDynamicTopology
 from splib.topology.static import addStaticTopology
 from splib.core.enum_types import ElementType
+from splib.core.utils import DEFAULT_VALUE
 from Sofa.Core import Object 
+
+
+class Geometry(BasePrefab):...
+
+@dataclasses.dataclass
+class InternalDataProvider(object):
+    positions : Any
+    # Topology information
+    edges : Any = DEFAULT_VALUE
+    triangles : Any  = DEFAULT_VALUE
+    quads : Any  = DEFAULT_VALUE
+    tetrahedra : Any  = DEFAULT_VALUE
+    hexahedra : Any  = DEFAULT_VALUE
+
+    def generateAttribute(self, parent : Geometry):
+        pass
+
 
 @dataclasses.dataclass
 class GeometryParameters(BaseParameters):
-    @dataclasses.dataclass
-    class Data(object):
-        # Initial positions
-        positions : Any
 
-        # Type of the highest degree element
-        elementType : Optional[ElementType]
-
-        # Topology information
-        edges : Optional[Any]
-        triangles : Optional[Any]
-        quads : Optional[Any]
-        tetrahedra : Optional[Any]
-        hexahedra : Optional[Any]
+    # Type of the highest degree element
+    elementType : Optional[ElementType]
 
     dynamicTopology : bool = False
-    data : Data
+    data : Optional[InternalDataProvider]
+
+
 
 class Geometry(BasePrefab):
     container :  Object # This should be more specialized into the right SOFA type
     modifier : Optional[Object]
 
+    params : GeometryParameters
+
     def __init__(self, params: GeometryParameters):
         BasePrefab.__init__(self, params)
+        self.params = params 
+        if params.data is not None :
+            params.data.generateAttribute(self)
         if(params.dynamicTopology):
             if(params.elementType is not None):
-                addDynamicTopology(self, **dataclasses.asdict(params.data), **params.kargs)
+                addDynamicTopology(self, container = {dataclasses.asdict(params.data)})
             else:
                 raise ValueError
         else:
-            addStaticTopology(self, **dataclasses.asdict(params.data), **params.kargs)
+            addStaticTopology(self, container = {dataclasses.asdict(params.data)})
--- stlib/geometry/file.py
+++ stlib/geometry/file.py
@@ -1,27 +1,31 @@
-from stlib.geometry import GeometryParameters
+from stlib.geometry import GeometryParameters, InternalDataProvider, Geometry
+from stlib.core.baseParameters import dataclasses
 from splib.topology.loader import loadMesh
+from splib.core.enum_types import ElementType
+
 from Sofa.Core import Node
 
+@dataclasses.dataclass
+class FileInternalDataProvider(InternalDataProvider):
+    filename : str
+
+    def __post_init__(self, **kwargs):
+        InternalDataProvider.__init__(self,**kwargs)
+
+    def generateAttribute(self, parent : Geometry):        
+    
+        loadMesh(parent, self.filename)
+
+        self.positions = parent.loader.position.linkpath
+        self.edges = parent.loader.edges.linkpath
+        self.triangles = parent.loader.triangles.linkpath
+        self.quads = parent.loader.quads.linkpath
+        self.hexaedra = parent.loader.hexaedra.linkpath
+        self.tetrahedra = parent.loader.tetras.linkpath
+
+
+
 class FileParameters(GeometryParameters):
-    def __init__(self,  filename, dynamicTopology = False):
-
-        customGeom = FileParameters.createData(filename)
-        GeometryParameters.__init__(data = customGeom, dynamicTopology = dynamicTopology)
-
-    @staticmethod
-    def createData(filename) -> GeometryParameters.Data :
-        data = GeometryParameters.Data()
-        
-        node = Sofa.Core.Node()
-        loadMesh(node, filename)
-        node.init()
-
-        data.positions = node.loader.position.value
-        data.edges = node.loader.edges.value
-        data.triangles = node.loader.triangles.value
-        data.tetrahedra = node.loader.tetras.value
-
-        node.cleanUp()
-
-        return data
+    def __init__(self,  filename, dynamicTopology = False, elementType : ElementType = None ):
+        GeometryParameters.__init__(data = FileInternalDataProvider(filename), dynamicTopology = dynamicTopology, elementType = elementType)
         
