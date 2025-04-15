f6f5946fbb060f918eaaa3260063542bbb76d651
bakpaul <paul.baksic@outlook.fr>
2025-04-15 15:48:01 +0200

    Fix visual example

--- stlib/geometry/file.py
+++ stlib/geometry/file.py
@@ -7,7 +7,7 @@ from Sofa.Core import Node
 
 @dataclasses.dataclass
 class FileInternalDataProvider(InternalDataProvider):
-    filename : str
+    filename : str = "mesh/cube.obj"
 
     def __post_init__(self, **kwargs):
         InternalDataProvider.__init__(self,**kwargs)
@@ -16,16 +16,16 @@ class FileInternalDataProvider(InternalDataProvider):
     
         loadMesh(parent, self.filename)
 
-        self.positions = parent.loader.position.linkpath
-        self.edges = parent.loader.edges.linkpath
-        self.triangles = parent.loader.triangles.linkpath
-        self.quads = parent.loader.quads.linkpath
-        self.hexaedra = parent.loader.hexaedra.linkpath
-        self.tetrahedra = parent.loader.tetras.linkpath
+        self.position = str(parent.loader.position.linkpath)
+        self.edges = str(parent.loader.edges.linkpath)
+        self.triangles = str(parent.loader.triangles.linkpath)
+        self.quads = str(parent.loader.quads.linkpath)
+        self.hexahedra = str(parent.loader.hexahedra.linkpath)
+        self.tetrahedra = str(parent.loader.tetras.linkpath)
 
 
 
 class FileParameters(GeometryParameters):
     def __init__(self,  filename, dynamicTopology = False, elementType : ElementType = None ):
-        GeometryParameters.__init__(data = FileInternalDataProvider(filename), dynamicTopology = dynamicTopology, elementType = elementType)
+        GeometryParameters.__init__(self,data = FileInternalDataProvider(filename), dynamicTopology = dynamicTopology, elementType = elementType)
         
