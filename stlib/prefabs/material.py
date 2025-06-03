af5b694786c11a8e0a90bd83a44b1595f9273874
hugtalbot <hugo.talbot@sofa-framework.org>
2025-06-03 16:16:11 +0200

    before the train 3/3

---
+++ stlib/prefabs/material.py
@@ -0,0 +1,26 @@
+from stlib.core.baseParameters import BaseParameters, Callable, Optional, dataclasses, Any
+from splib.core.enum_types import StateType
+from stlib.core.basePrefab import BasePrefab
+from splib.mechanics.mass import addMass
+
+@dataclasses.dataclass
+class MaterialParameters(BaseParameters):
+    name: str = "Material"
+
+    stateType: StateType = StateType.VEC3
+    massDensity: float
+    massLumping: bool
+
+    addMaterial : Optional[Callable] = lambda node : addMass(node, node.stateType, massDensity=node.massDensity, lumping=node.massLumping)
+
+
+# TODO : previously called Behavior
+class Material(BasePrefab):
+    def __init__(self, params: MaterialParameters):
+        BasePrefab.__init__(self, params)
+
+        self.params = params
+        self.addObject("MechanicalObject", name="States", template=str(self.params.stateType))
+
+        if(params.addMaterial is not None):
+            params.addMaterial(self)
