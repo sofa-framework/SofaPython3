ce247a442231a57539279f462317b21c7dfea753
hugtalbot <hugo.talbot@sofa-framework.org>
2025-06-03 16:16:11 +0200

    before the train 3/3

--- stlib/entities/deformable/__deformable__.py
+++ stlib/entities/deformable/__deformable__.py
@@ -1,37 +1,69 @@
 from stlib.entities import Entity, EntityParameters
-from stlib.entities.deformable.__parameters__ import  DeformableParameters
+from stlib.prefabs.material import Material, MaterialParameters
 from stlib.prefabs.visual import Visual
-from stlib.mixins.collision import CollisionMixin
-from splib.core.enum_types import ConstitutiveLaw
+from splib.core.enum_types import ConstitutiveLaw, ElementType
+from splib.mechanics.linear_elasticity import *
+from splib.mechanics.hyperelasticity import *
+from splib.mechanics.mass import addMass
 
-class DeformableParameters(EntityParameters):
 
-    constitutiveLaw : ConstitutiveLaw = ConstitutiveLaw.ELASTIC
+class DeformableBehaviorParameters(MaterialParameters):
 
-class Deformable(CollisionMixin, Entity):
+    constitutiveLawType : ConstitutiveLaw 
+    elementType : ElementType
+    parameters : list[float]
 
-    params : DeformableParameters
+    def addMaterial(self, node):
 
-    @staticmethod
-    def getParameters(**kwargs) -> DeformableParameters:
-        return DeformableParameters(**kwargs)
+        addMass(node, node.stateType, massDensity=node.massDensity, lumping=node.massLumping)
+
+        # TODO : change this with inheritance
+        if(self.constitutiveLawType == ConstitutiveLaw.HYPERELASTIC):
+            addHyperelasticity(node,self.elementType, self.parameters)
+        else:
+            addLinearElasticity(node,self.elementType, self.parameters[0], self.parameters[1])
+
+
+# class Deformable(Entity):
+
+#     params : DeformableParameters
+
+#     @staticmethod
+#     def getParameters(**kwargs) -> DeformableParameters:
+#         return DeformableParameters(**kwargs)
         
 
-    def __init__(self, params : DeformableParameters, **kwargs):
-        Entity.__init__(self, **kwargs)  
+#     def __init__(self, params : DeformableParameters, **kwargs):
+#         Entity.__init__(self, **kwargs)  
         
-        self.__addConstitutiveLaw__()
-        self.addCollision(params.collision)
+#         self.__addConstitutiveLaw__()
+#         self.addCollision(params.collision)
 
 
-    #@customizable
-    # Need generic way of defining paramaters (linear/hyper...)
-    def __addConstitutiveLaw__(self):
-        self.params.addConstitutiveLaw()
+#     #@customizable
+#     # Need generic way of defining paramaters (linear/hyper...)
+#     def __addConstitutiveLaw__(self):
+#         self.params.addConstitutiveLaw()
 
     
-    #@customizable
-    def __addVisual__(self):
-        #Extract surface and add identity mapping
-        self.add(Visual, self.params.visual)
+#     #@customizable
+#     def __addVisual__(self):
+#         #Extract surface and add identity mapping
+#         self.add(Visual, self.params.visual)
 
+
+
+
+
+def createScene(root) :
+
+    from stlib.geometry.file import FileParameters    
+    from stlib.geometry.extract import ExtractParameters    
+
+    liverParameters = EntityParameters()
+    liverParameters.behavior = DeformableBehaviorParameters()
+    liverParameters.behavior.constitutiveLawType = ConstitutiveLaw.ELASTIC
+    liverParameters.behavior.parameters = [1000, 0.45]
+    liverParameters.geometry = FileParameters("liver.vtk")
+    liverParameters.visual = ExtractParameters()
+    myDeformableObject = root.add(Entity, liverParameters)
