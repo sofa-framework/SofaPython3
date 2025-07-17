7468b96692ccf41a636927d8021fff4312e25b7c
hugtalbot <hugo.talbot@sofa-framework.org>
2025-07-17 16:44:15 +0200

    update addMass using elementType

--- splib/mechanics/mass.py
+++ splib/mechanics/mass.py
@@ -3,18 +3,21 @@ from splib.core.utils import defaultValueType, DEFAULT_VALUE, isDefault
 from splib.core.enum_types import ElementType
 
 
-# TODO : use the massDensity only and deduce totalMass if necessary from it + volume
+# TODO : use the massDensity ONLY and deduce totalMass if necessary from it + volume
+
 @ReusableMethod
-def addMass(node, template, totalMass=DEFAULT_VALUE, massDensity=DEFAULT_VALUE, lumping=DEFAULT_VALUE, **kwargs):
+def addMass(node, elem:ElementType, totalMass=DEFAULT_VALUE, massDensity=DEFAULT_VALUE, lumping=DEFAULT_VALUE, **kwargs):
     if (not isDefault(totalMass)) and (not isDefault(massDensity)) :
         print("[warning] You defined the totalMass and the massDensity in the same time, only taking massDensity into account")
-        kwargs.pop('massDensity')
+        del kwargs["massDensity"]
 
-    # if(template=="Rigid3"):
-    node.addObject("UniformMass",name="mass", totalMass=totalMass, massDensity=massDensity, lumping=lumping, **kwargs)
-    # else:
-        # node.addObject("MeshMatrixMass",name="mass", totalMass=totalMass, massDensity=massDensity, lumping=lumping, **kwargs)
+    if(elem !=ElementType.POINTS and elem !=ElementType.EDGES):
+        node.addObject("MeshMatrixMass",name="mass", totalMass=totalMass, massDensity=massDensity, lumping=lumping, **kwargs)
+    else:
+        if (not isDefault(massDensity)) :
+            print("[warning] mass density can only be used on a surface or volumetric topology. Please use totalMass instead")
+        if (not isDefault(lumping)) :
+            print("[warning] lumping can only be set for surface or volumetric topology")
 
-
-
+        node.addObject("UniformMass",name="mass", totalMass=totalMass, **kwargs)
 
