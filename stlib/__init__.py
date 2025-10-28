270d86700510672547a729b1fd7b1d505704fd2f
Paul Baksic <paul.baksic@outlook.fr>
2025-10-28 16:09:50 +0100

    Restore checks on name and add check on the presence of parameters

--- stlib/__init__.py
+++ stlib/__init__.py
@@ -1,6 +1,7 @@
 __all__ = ["core","entities","geometries","materials","collision","visual"]
 
 import Sofa.Core
+from stlib.core.basePrefab import BasePrefab
 
 def __genericAdd(self : Sofa.Core.Node, typeName, **kwargs):
     def findName(cname, names):
@@ -12,30 +13,42 @@ def __genericAdd(self : Sofa.Core.Node, typeName, **kwargs):
             rname = cname + str(i+1)
         return rname
 
+
+    def checkName(context : Sofa.Core.Node, name):
+        # Check if the name already exists, if this happens, create a new one.
+        if name in context.children or name in context.objects:
+            names = {node.name.value for node in context.children}
+            names = names.union({object.name.value for object in context.objects})
+            name = findName(name, names)
+        return name
+
+
     # Check if a name is provided, if not, use the one of the class
     params = kwargs.copy()
-    # if "name" not in params:
-    #     if isinstance(typeName, str):
-    #         params["name"] = typeName
-    #     elif isinstance(typeName, type) and issubclass(typeName, Sofa.Core.Node):
-    #         params["name"] = typeName.__name__
-    #     elif isinstance(typeName, Sofa.Core.Node):
-    #         params["name"] = "Node"
-    #     elif isinstance(typeName, type) and issubclass(typeName, Sofa.Core.Object):
-    #         params["name"] = typeName.name.value
-    #     elif isinstance(typeName, type) and issubclass(typeName, Sofa.Core.ObjectDeclaration):
-    #         params["name"] = typeName.__name__
-    #     else:
-    #         raise RuntimeError("Invalid argument ", typeName)
+    if isinstance(typeName, type) and issubclass(typeName, BasePrefab): #Only for prefabs
+        if "parameters" not in params or len(params.keys()) > 1:
+            raise RuntimeError("Invalid argument, a prefab takes only the \"parameters\" kwargs as input")
+    elif "name" not in params : #This doesn't apply to prefab
+        if isinstance(typeName, str):
+            params["name"] = typeName
+        elif isinstance(typeName, type) and issubclass(typeName, Sofa.Core.Node):
+            params["name"] = typeName.__name__
+        elif isinstance(typeName, Sofa.Core.Node):
+            params["name"] = "Node"
+        elif isinstance(typeName, type) and issubclass(typeName, Sofa.Core.Object):
+            params["name"] = typeName.name.value
+        elif isinstance(typeName, type) and issubclass(typeName, Sofa.Core.ObjectDeclaration):
+            params["name"] = typeName.__name__
+        else:
+            raise RuntimeError("Invalid argument ", typeName)
 
-    # # Check if the name already exists, if this happens, create a new one.
-    # if params["name"] in self.children or params["name"] in self.objects:
-    #     names = {node.name.value for node in self.children}
-    #     names = names.union({object.name.value for object in self.objects})
-    #     params["name"] = findName(params["name"], names)
+    if isinstance(typeName, type) and issubclass(typeName, BasePrefab):
+        params["parameters"].name = checkName(self, params["parameters"].name)
+    else:
+        params["name"] = checkName(self, params["name"])
 
     # Dispatch the creation to either addObject or addChild
-    if isinstance(typeName, type) and issubclass(typeName, core.basePrefab.BasePrefab):
+    if isinstance(typeName, type) and issubclass(typeName, BasePrefab):
         pref = self.addChild(typeName(**params))
         pref.init()
     elif isinstance(typeName, Sofa.Core.Node):
