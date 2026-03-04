835d78d8fe8f61b7cfb01b88673314d69a7a4241
Alex Bilger <alxbilger@users.noreply.github.com>
2026-03-04 10:02:30 +0100

    Add binding for `GetVersion` and associated unit tests (#583)
    * Add binding for `GetVersion` and associated unit tests
    
    Introduce a static method `GetVersion` in `Sofa.Helper.Utils` to retrieve the SOFA version in `vMM.mm` format. Added unit tests in `Helper/Version.py` to verify existence and format adherence. Updated CMakeLists accordingly.
    
    * make the version string static
    
    * move GetVersion from Utils to Version
    
    * add GetVersion() to the Sofa package

---
+++ bindings/Sofa/tests/Helper/Version.py
@@ -0,0 +1,26 @@
+import Sofa
+import Sofa.Helper
+import unittest
+
+class GetVersionTest(unittest.TestCase):
+    """
+    Contains unit tests for verifying the existence and format of the `GetVersion` method
+    in the `Sofa.Helper.Utils` module.
+
+    Includes tests to assert that the version is correctly formatted and meets the expected
+    structure.
+    """
+    def test_exist(self):
+        self.assertTrue(hasattr(Sofa.Helper, "GetVersion"))
+
+    def test_version_format(self):
+        version = Sofa.Helper.GetVersion()
+        print(f"SOFA Version: {version}")
+        self.assertTrue(version.startswith('v'))
+        version = version.replace('v', '')
+        self.assertTrue(version.count('.') == 1)
+        major, minor = version.split('.')
+        self.assertTrue(major.isdigit())
+        self.assertTrue(minor.isdigit())
+        self.assertEqual(len(major), 2)
+        self.assertEqual(len(minor), 2)
